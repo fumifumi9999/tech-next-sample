17fb6e6ae3fccc16faf222f142cfd0af57b1b1f6
Fumi <fumi9999@gmail.com>
2023-11-09 01:27:35 +0900

    First commit

---
+++ src/app/middleware1/middleware2/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Middleware2Page = () => {
+  return (
+    <div>Middleware2Page</div>
+  )
+}
+
+export default Middleware2Page
---
+++ src/app/middleware1/page.jsx
@@ -0,0 +1,5 @@
+export default async function Middleware1Page() {
+
+    return <h1 className="text-5xl">Middleware1Page!</h1>
+
+}
